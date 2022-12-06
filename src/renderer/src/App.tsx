5a33f54641470d7f32b56ffc4ba80099781d282c
amanda-santos <amandasf0200@gmail.com>
2022-12-06 19:21:04 -0300

    chore: ➕ add tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react'
+import './styles/global.css'
 
 export const App = (): ReactElement => {
-  return <h1 style={{ fontFamily: 'Inter' }}>Hello World</h1>
+  return <h1 className="text-violet-400 text-2xl font-bold">Hello World</h1>
 }
