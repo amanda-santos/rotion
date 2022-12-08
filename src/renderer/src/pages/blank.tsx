a55b9a7cdaec58e1bf5b117821db2b783c3fcd41
amanda-santos <amandasf0200@gmail.com>
2022-12-08 17:48:50 -0300

    feat: ✨ add Document page

--- src/renderer/src/pages/blank.tsx
+++ src/renderer/src/pages/blank.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-export function Blank() {
+export const Blank = () => {
   return (
     <main className="flex-1 flex items-center justify-center text-rotion-400">
       Select or create a document
--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,37 @@
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
-export function Document() {
+export const Document = () => {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Document
-      <Link to="/">Go back</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs">
+          TABLE OF CONTENTS
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Node.js</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis
+        animi architecto! Doloremque earum itaque repudiandae hic ipsum eligendi
+        adipisci. Fugit consequatur impedit dicta pariatur et quia, perferendis
+        assumenda inventore! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. In perspiciatis animi architecto! Doloremque earum itaque
+        repudiandae hic ipsum eligendi adipisci. Fugit consequatur impedit dicta
+        pariatur et quia, perferendis assumenda inventore! Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. In perspiciatis animi architecto!
+        Doloremque earum itaque repudiandae hic ipsum eligendi adipisci. Fugit
+        consequatur impedit dicta pariatur et quia, perferendis assumenda
+        inventore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+        perspiciatis animi architecto! Doloremque earum itaque repudiandae hic
+        ipsum eligendi adipisci. Fugit consequatur impedit dicta pariatur et
+        quia, perferendis assumenda inventore!
+      </section>
     </main>
   )
 }
