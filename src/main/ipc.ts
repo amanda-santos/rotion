864d8e6bc2b79011321541d6f1e3f1be07656fa4
amanda-santos <amandasf0200@gmail.com>
2022-12-09 18:41:46 -0300

    feat: ✨ add CRUD operations on IPC main

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,34 +1,71 @@
 import { ipcMain } from 'electron'
+import { randomUUID } from 'crypto'
 
 import { IPC } from '@shared/constants/ipc'
-import { FetchAllDocumentsResponse } from '@shared/types/ipc'
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  FetchDocumentRequest,
+  FetchAllDocumentsResponse,
+  FetchDocumentResponse,
+  Document,
+  SaveDocumentRequest,
+} from '@shared/types/ipc'
+import { store } from './store'
+
+ipcMain.handle(
+  IPC.DOCUMENTS.CREATE,
+  async (): Promise<CreateDocumentResponse> => {
+    const id = randomUUID()
+
+    const document: Document = {
+      id,
+      title: 'Untitled',
+    }
+
+    store.set(`documents.${id}`, document)
+
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.DELETE,
+  async (_, { id }: DeleteDocumentRequest): Promise<void> => {
+    // @ts-ignore (https://github.com/sindresorhus/electron-store/issues/196)
+    store.delete(`documents.${id}`)
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH,
+  async (_, { id }: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    const document: Document = store.get(`documents.${id}`)
+
+    return {
+      data: document,
+    }
+  },
+)
 
 ipcMain.handle(
   IPC.DOCUMENTS.FETCH_ALL,
   async (): Promise<FetchAllDocumentsResponse> => {
     return {
-      data: [
-        {
-          id: '1',
-          title: 'Document 1',
-          content: '',
-        },
-        {
-          id: '2',
-          title: 'Document 2',
-          content: '',
-        },
-        {
-          id: '3',
-          title: 'Document 3',
-          content: '',
-        },
-        {
-          id: '4',
-          title: 'Document 4',
-          content: '',
-        },
-      ],
+      data: Object.values(store.get('documents')),
     }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.SAVE,
+  async (_, { id, title, content }: SaveDocumentRequest): Promise<void> => {
+    store.set(`documents.${id}`, {
+      id,
+      title,
+      content,
+    })
   },
 )
